e399abfb45f0ae45d39705e80597b0353d173dc2
wildanarkan <cillmystic@gmail.com>
2024-02-03 23:10:11 +0700

    feat: Create & Read SQFLite

---
+++ lib/create.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:latihan_sqflite/database_instance.dart';
+import 'package:sqflite/sqflite.dart';
+
+class CreateScreen extends StatefulWidget {
+  const CreateScreen({super.key});
+
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  DatabaseInstance databaseInstance = DatabaseInstance();
+  TextEditingController nameC = TextEditingController();
+  TextEditingController categoryC = TextEditingController();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    databaseInstance.database();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("CREATE"),),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text("Name Product"),
+            TextField(
+              controller: nameC,
+            ),
+            SizedBox(height: 20,),
+            Text("Category"),
+            TextField(
+              controller: categoryC,
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: () async{
+              await databaseInstance.insert(
+                {
+                  'name': nameC.text,
+                  'category': categoryC.text,
+                  'created_at': DateTime.now().toString(),
+                  'update_at': DateTime.now().toString(),
+                }
+              );
+              Navigator.pop(context);
+              setState(() {
+                
+              });
+            }, child: Text("SUBMIT"))
+          ],
+        ),
+      ),
+    );
+  }
+}
